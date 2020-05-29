8a71da53edfd9034f71a57324fc6bcf3ec5fff0e
amosmoyo <amosmoyo5300@gmail.com>
2020-05-29 10:33:59 +0300

    new changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = {
-  database: process.env.MONGO_URL || 'mongodb+srv://amosmoyo:qiGlmy7Q@cluster0-spg3d.mongodb.net/sco207', 
+  database: 'mongodb+srv://amosmoyo:qiGlmy7Q@cluster0-spg3d.mongodb.net/sco207?retryWrites=true&w=majority',
+  // mongodb+srv://amosmoyo:<password>@cluster0-spg3d.mongodb.net/test 
+  // mongodb+srv://amosmoyo:<password>@cluster0-spg3d.mongodb.net/test?retryWrites=true&w=majority
   //database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'coding is my hobby, bae, hahaha'
 }
